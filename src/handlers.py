d714a7dcccaa7bc2bfcf60cab2b4aaa571eedee0
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-06-19 08:35:57 +0000

    Move to Poetry and setup pre-commit

--- src/handlers.py
+++ src/handlers.py
@@ -3,188 +3,168 @@ from functools import partial
 from telegram import Update
 from telegram.ext import ContextTypes
 
-from constants import (
-    ADMIN_USER_IDS,
-
-    COMMAND_MESSAGES,
-
-    BACK_TO_LIST_MARKUP,
-    COMMANDS_MARKUP,
-    SHOW_MORE_MARKUP,
-)
-
-from processors import (
-    tafrigh_processor,
-    transcriptions_processor,
-    hadiths_processor,
-    shamela_semantic_processor,
-    shamela_classical_processor,
-)
+from constants import ADMIN_USER_IDS, BACK_TO_LIST_MARKUP, COMMAND_MESSAGES, COMMANDS_MARKUP, SHOW_MORE_MARKUP
+from processors import (hadiths_processor, shamela_classical_processor, shamela_semantic_processor, tafrigh_processor,
+                        transcriptions_processor)
 
 
 async def enable_bot_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if update.message.from_user.id in ADMIN_USER_IDS:
-        context.bot_data['enabled'] = True
+  if update.message.from_user.id in ADMIN_USER_IDS:
+    context.bot_data['enabled'] = True
 
-        await update.message.reply_text(COMMAND_MESSAGES['bot_enabled_successfully'], reply_markup=BACK_TO_LIST_MARKUP)
+    await update.message.reply_text(COMMAND_MESSAGES['bot_enabled_successfully'], reply_markup=BACK_TO_LIST_MARKUP)
 
 
 async def disable_bot_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if update.message.from_user.id in ADMIN_USER_IDS:
-        context.bot_data['enabled'] = False
+  if update.message.from_user.id in ADMIN_USER_IDS:
+    context.bot_data['enabled'] = False
 
-        await update.message.reply_text(COMMAND_MESSAGES['bot_disabled_successfully'], reply_markup=BACK_TO_LIST_MARKUP)
+    await update.message.reply_text(COMMAND_MESSAGES['bot_disabled_successfully'], reply_markup=BACK_TO_LIST_MARKUP)
 
 
 async def start_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.bot_data['enabled']:
-        await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
-        return
+  if not context.bot_data['enabled']:
+    await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
+    return
 
-    await update.message.reply_html(COMMAND_MESSAGES['start'], reply_markup=COMMANDS_MARKUP)
+  await update.message.reply_html(COMMAND_MESSAGES['start'], reply_markup=COMMANDS_MARKUP)
 
 
 async def tafrigh_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.bot_data['enabled']:
-        await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
-        return
+  if not context.bot_data['enabled']:
+    await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
+    return
 
-    await command_reply_handler(update, context, 'tafrigh')
+  await command_reply_handler(update, context, 'tafrigh')
 
 
 async def transcriptions_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.bot_data['enabled']:
-        await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
-        return
+  if not context.bot_data['enabled']:
+    await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
+    return
 
-    await command_reply_handler(update, context, 'transcriptions')
+  await command_reply_handler(update, context, 'transcriptions')
 
 
 async def hadiths_semantic_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.bot_data['enabled']:
-        await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
-        return
+  if not context.bot_data['enabled']:
+    await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
+    return
 
-    await command_reply_handler(update, context, 'hadiths_semantic')
+  await command_reply_handler(update, context, 'hadiths_semantic')
 
 
 async def shamela_semantic_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.bot_data['enabled']:
-        await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
-        return
+  if not context.bot_data['enabled']:
+    await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
+    return
 
-    await command_reply_handler(update, context, 'shamela_semantic')
+  await command_reply_handler(update, context, 'shamela_semantic')
 
 
 async def hadiths_classical_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.bot_data['enabled']:
-        await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
-        return
+  if not context.bot_data['enabled']:
+    await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
+    return
 
-    await command_reply_handler(update, context, 'hadiths_classical')
+  await command_reply_handler(update, context, 'hadiths_classical')
 
 
 async def shamela_classical_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.bot_data['enabled']:
-        await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
-        return
+  if not context.bot_data['enabled']:
+    await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
+    return
 
-    await command_reply_handler(update, context, 'shamela_classical')
+  await command_reply_handler(update, context, 'shamela_classical')
 
 
 async def text_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    if not context.bot_data['enabled']:
-        await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
-        return
+  if not context.bot_data['enabled']:
+    await update.message.reply_text(COMMAND_MESSAGES['bot_is_not_enabled'], reply_markup=BACK_TO_LIST_MARKUP)
+    return
 
-    await commands_handler(update, context, show_more_button=True)
+  await commands_handler(update, context, show_more_button=True)
 
 
 async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    query = update.callback_query
+  query = update.callback_query
 
-    if not context.bot_data['enabled']:
-        await context.bot.send_message(
-            chat_id=query.from_user.id,
-            text=COMMAND_MESSAGES['bot_is_not_enabled'],
-            reply_markup=BACK_TO_LIST_MARKUP,
-        )
-        return
+  if not context.bot_data['enabled']:
+    await context.bot.send_message(
+        chat_id=query.from_user.id,
+        text=COMMAND_MESSAGES['bot_is_not_enabled'],
+        reply_markup=BACK_TO_LIST_MARKUP,
+    )
+    return
 
-    # CallbackQueries need to be answered, even if no notification to the user is needed
-    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
-    await query.answer()
+  # CallbackQueries need to be answered, even if no notification to the user is needed
+  # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+  await query.answer()
 
-    if query.data == 'back_to_list':
-        await query.edit_message_text(COMMAND_MESSAGES['start'], reply_markup=COMMANDS_MARKUP, parse_mode='HTML')
-    elif query.data == 'show_more':
-        await commands_handler(
-            update,
-            context,
-            show_more_button=True,
-            show_more_results=True,
-            user_id=query.from_user.id,
-        )
-    else:
-        context.user_data['command'] = query.data
+  if query.data == 'back_to_list':
+    await query.edit_message_text(COMMAND_MESSAGES['start'], reply_markup=COMMANDS_MARKUP, parse_mode='HTML')
+  elif query.data == 'show_more':
+    await commands_handler(update, context, show_more_button=True, show_more_results=True, user_id=query.from_user.id)
+  else:
+    context.user_data['command'] = query.data
 
-        await query.edit_message_text(COMMAND_MESSAGES[query.data], reply_markup=BACK_TO_LIST_MARKUP, parse_mode='HTML')
+    await query.edit_message_text(COMMAND_MESSAGES[query.data], reply_markup=BACK_TO_LIST_MARKUP, parse_mode='HTML')
 
 
 async def command_reply_handler(update: Update, context: ContextTypes.DEFAULT_TYPE, command: str) -> None:
-    context.user_data['command'] = command
+  context.user_data['command'] = command
 
-    await update.message.reply_html(COMMAND_MESSAGES[command], reply_markup=BACK_TO_LIST_MARKUP)
+  await update.message.reply_html(COMMAND_MESSAGES[command], reply_markup=BACK_TO_LIST_MARKUP)
 
 
 async def commands_handler(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE,
-    show_more_button: bool=False,
-    show_more_results: bool=False,
-    user_id: int=-1,
+    show_more_button: bool = False,
+    show_more_results: bool = False,
+    user_id: int = -1,
 ) -> None:
-    if user_id != -1:
-        reply_method = partial(context.bot.send_message, chat_id=user_id)
+  if user_id != -1:
+    reply_method = partial(context.bot.send_message, chat_id=user_id)
+  else:
+    reply_method = update.message.reply_text
+
+  show_commands_list = True
+
+  if (command := context.user_data.get('command', None)):
+    if show_more_results:
+      context.user_data['results_start'] = context.user_data['results_start'] + 5
     else:
-        reply_method = update.message.reply_text
+      context.user_data['results_start'] = 0
+      context.user_data['message'] = update.message.text
 
-    show_commands_list = True
-
-    if (command := context.user_data.get('command', None)):
-        if show_more_results:
-            context.user_data['results_start'] = context.user_data['results_start'] + 5
-        else:
-            context.user_data['results_start'] = 0
-            context.user_data['message'] = update.message.text
-
-        if show_more_results:
-            await reply_method(text=COMMAND_MESSAGES['getting_more_results'])
-        else:
-            await reply_method(text=COMMAND_MESSAGES['wait_for_search'])
-
-        match command:
-            case 'tafrigh':
-                show_more_button = await tafrigh_processor(update)
-            case 'transcriptions':
-                show_more_button = await transcriptions_processor(context, reply_method)
-            case 'hadiths_semantic':
-                show_more_button = await hadiths_processor('semantic', context, reply_method)
-            case 'shamela_semantic':
-                show_more_button = await shamela_semantic_processor(context, reply_method)
-            case 'hadiths_classical':
-                show_more_button = await hadiths_processor('classical', context, reply_method)
-            case 'shamela_classical':
-                show_more_button = await shamela_classical_processor(context, reply_method)
-            case _:
-                await reply_method(text=COMMAND_MESSAGES['no_command_selected'], reply_markup=COMMANDS_MARKUP)
-                show_commands_list = False
+    if show_more_results:
+      await reply_method(text=COMMAND_MESSAGES['getting_more_results'])
     else:
+      await reply_method(text=COMMAND_MESSAGES['wait_for_search'])
+
+    match command:
+      case 'tafrigh':
+        show_more_button = await tafrigh_processor(update)
+      case 'transcriptions':
+        show_more_button = await transcriptions_processor(context, reply_method)
+      case 'hadiths_semantic':
+        show_more_button = await hadiths_processor('semantic', context, reply_method)
+      case 'shamela_semantic':
+        show_more_button = await shamela_semantic_processor(context, reply_method)
+      case 'hadiths_classical':
+        show_more_button = await hadiths_processor('classical', context, reply_method)
+      case 'shamela_classical':
+        show_more_button = await shamela_classical_processor(context, reply_method)
+      case _:
         await reply_method(text=COMMAND_MESSAGES['no_command_selected'], reply_markup=COMMANDS_MARKUP)
         show_commands_list = False
+  else:
+    await reply_method(text=COMMAND_MESSAGES['no_command_selected'], reply_markup=COMMANDS_MARKUP)
+    show_commands_list = False
 
-    if show_commands_list:
-        if show_more_button:
-            await reply_method(text=COMMAND_MESSAGES['show_more_continue_or_show_list'], reply_markup=SHOW_MORE_MARKUP)
-        else:
-            await reply_method(text=COMMAND_MESSAGES['continue_or_show_list'], reply_markup=BACK_TO_LIST_MARKUP)
+  if show_commands_list:
+    if show_more_button:
+      await reply_method(text=COMMAND_MESSAGES['show_more_continue_or_show_list'], reply_markup=SHOW_MORE_MARKUP)
+    else:
+      await reply_method(text=COMMAND_MESSAGES['continue_or_show_list'], reply_markup=BACK_TO_LIST_MARKUP)
--- src/main.py
+++ src/main.py
@@ -1,45 +1,34 @@
 from telegram.ext import Application, CallbackQueryHandler, CommandHandler, MessageHandler, filters
 
 from constants import TELEGRAM_BOT_TOKEN
-
-from handlers import (
-    enable_bot_handler,
-    disable_bot_handler,
-    start_handler,
-    tafrigh_handler,
-    transcriptions_handler,
-    hadiths_semantic_handler,
-    shamela_semantic_handler,
-    hadiths_classical_handler,
-    shamela_classical_handler,
-    text_handler,
-    button_handler,
-)
+from handlers import (button_handler, disable_bot_handler, enable_bot_handler, hadiths_classical_handler,
+                      hadiths_semantic_handler, shamela_classical_handler, shamela_semantic_handler, start_handler,
+                      tafrigh_handler, text_handler, transcriptions_handler)
 
 
 def main():
-    application = Application.builder().token(TELEGRAM_BOT_TOKEN).post_init(post_init).build()
+  application = Application.builder().token(TELEGRAM_BOT_TOKEN).post_init(post_init).build()
 
-    application.add_handler(CommandHandler('enable_bot', enable_bot_handler))
-    application.add_handler(CommandHandler('disable_bot', disable_bot_handler))
+  application.add_handler(CommandHandler('enable_bot', enable_bot_handler))
+  application.add_handler(CommandHandler('disable_bot', disable_bot_handler))
 
-    application.add_handler(CommandHandler('start', start_handler))
-    application.add_handler(CommandHandler('tafrigh', tafrigh_handler))
-    application.add_handler(CommandHandler('transcriptions', transcriptions_handler))
-    application.add_handler(CommandHandler('hadiths_semantic', hadiths_semantic_handler))
-    application.add_handler(CommandHandler('shamela_semantic', shamela_semantic_handler))
-    application.add_handler(CommandHandler('hadiths_classical', hadiths_classical_handler))
-    application.add_handler(CommandHandler('shamela_classical', shamela_classical_handler))
+  application.add_handler(CommandHandler('start', start_handler))
+  application.add_handler(CommandHandler('tafrigh', tafrigh_handler))
+  application.add_handler(CommandHandler('transcriptions', transcriptions_handler))
+  application.add_handler(CommandHandler('hadiths_semantic', hadiths_semantic_handler))
+  application.add_handler(CommandHandler('shamela_semantic', shamela_semantic_handler))
+  application.add_handler(CommandHandler('hadiths_classical', hadiths_classical_handler))
+  application.add_handler(CommandHandler('shamela_classical', shamela_classical_handler))
 
-    application.add_handler(MessageHandler(filters.TEXT, text_handler))
-    application.add_handler(CallbackQueryHandler(button_handler))
+  application.add_handler(MessageHandler(filters.TEXT, text_handler))
+  application.add_handler(CallbackQueryHandler(button_handler))
 
-    application.run_polling()
+  application.run_polling()
 
 
 async def post_init(application: Application) -> None:
-    application.bot_data['enabled'] = True
+  application.bot_data['enabled'] = True
 
 
 if __name__ == '__main__':
-    main()
+  main()
