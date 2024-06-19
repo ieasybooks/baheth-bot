82291f2b0e1528ef9287e74afc2cfb5ae61b2603
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-06-19 08:34:35 +0000

    Setup Google Chrome and ChromeDriver in DevContainer and prod container

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,6 @@
-pip install -r requirements.txt
+curl -sSL https://install.python-poetry.org | python -
+poetry config virtualenvs.create false
+poetry install
 
 test -f .env || cp .env.example .env
 
