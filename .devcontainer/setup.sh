0beeeecf2288efb835fa389715a3b01918739078
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-06-29 17:38:51 +0000

    Remove kamal setup in DevContainer as the image is emulated and docker-in-docker is not supporting this

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,5 +3,3 @@ poetry config virtualenvs.create false
 poetry install
 
 test -f .env || cp .env.example .env
-
-echo 'alias kamal='\''docker run -it --rm -v "${PWD}:/workdir" -v "${SSH_AUTH_SOCK}:/ssh-agent" -v /var/run/docker.sock:/var/run/docker.sock -e "SSH_AUTH_SOCK=/ssh-agent" ghcr.io/basecamp/kamal:v1.6.0'\''' >> ~/.zshrc
